4cb770311871915eaf76c7ff9cb43140aaf5e25d
Disha Bajaj <dishabajaj1508@gmail.com>
2023-02-20 00:07:59 +0530

    Created db query related functions

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -32,6 +32,39 @@ function db_select($select_query){
     return $rows;
 }
 
+function get_last_insert_id(){
+    $connection=db_connect();
+    return mysqli_insert_id($connection);
+}
+
+//Prevents mysql injection
+function sanitize($value){
+    $connection=db_connect();
+    return trim(mysqli_real_escape_string($connection,$value));
+}
+
+function old($collection,$key){
+    //Form mein koi value wrong hai toh right value bhi nikal jarahe the to prevent that..
+    return trim(isset($collection[$key])?$collection[$key]:'');
+}
+
+function prepare_insert_query($table_name,$data){
+    // '' commas for insert query for string ,phone no,address etc
+    $values=array_values($data);
+    for($i=0;$i<count($values);$i++){
+        $values[$i]="'".$values[$i]."'";
+    }
+
+    $columns=implode(", ",array_keys($data));
+    $values=implode(", ",$values);
+    $query="INSERT INTO $table_name($columns) VALUES($values)";
+    return $query;
+}
+function get_image_name($image_name,$id){
+    //If name contains only extension
+    return strpos($image_name,'.')?$image_name:"$id.$image_name";
+}
+
 function db_error(){
     $connection=db_connect();
     return mysqli_error($connection);
