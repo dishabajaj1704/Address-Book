e7eee2995f1f183e1722801e93227082bfa2c682
Disha Bajaj <dishabajaj1508@gmail.com>
2023-03-05 23:27:33 +0530

    Toast for operations done

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,3 +1,32 @@
-$(function() {
+$(function () {
     $('.modal').modal();
 });
+
+$(function () {
+    $('.modal').modal();
+    function getUrlQueryParams() {
+        const queryParams = window.location.search.substring(1).split("&");
+        let vars = [], hash;
+        for (let i = 0; i < queryParams.length; i++) {
+            hash = queryParams[i].split("=");
+            vars[hash[0]] = hash[1];
+        }
+        return vars;
+    }
+
+    const queryParams = getUrlQueryParams();
+    const operation = queryParams['op'];
+    const status = queryParams['status'];
+    if (operation === "add" && status === "success") {
+        M.toast({
+            html: 'Contact Added successfully',
+            classes: 'green darken-1'
+        });
+    }
+    else if (operation === "edit" && status === "success") {
+        M.toast({
+            html: 'Contact Updated successfully',
+            classes: 'green darken-1'
+        });
+    }
+})
