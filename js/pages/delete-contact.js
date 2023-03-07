af8db4b79ada0009ee59da2a11bbbc4da5406f05
Disha Bajaj <dishabajaj1508@gmail.com>
2023-03-07 14:46:35 +0530

    Passing id to agreeBtn logic

---
+++ js/pages/delete-contact.js
@@ -0,0 +1,10 @@
+
+$(".delete-element").click(function (evt) {
+
+    $id = $(this).data('id');
+    console.log($id);
+
+    $("#deleteModalAgreeBtn").attr('href', `delete-contact.php?id=${$id}`);
+
+});
+
